cf5fcb12266db04eaea562ed0ceb5ea1ebe97f71
RaphaelPellegrin <raphael.pellegrin74@gmail.com>
2020-11-23 16:36:13 +0000

    Update Pymc3.py

--- Experiments/Pymc3.py
+++ Experiments/Pymc3.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import warnings
 from pymc3 import Model
 import pymc3 as pm
+import theano.tensor as T
 
 
-def pymc3_sampling(D, sigma_in, out):
+def pymc3_sampling(D, sigma_in, out_last_hidden_layer):
 	"""
 	In
 	"""
@@ -14,10 +15,9 @@ def pymc3_sampling(D, sigma_in, out):
 	    # w has a prior: N(0,1) 
 	    w = pm.Normal('w', mu=0, sigma=sigma_in, shape=(D+1)) 
 	    linear_combi = pm.math.dot(X_subset,w)
-	    # TO CHECK SOFTMAX
-	    thetas = pm.Deterministic('theta', pm.math.softmax(linear_combi))
-	    # Y commes from a Categorical(theta)
-	    y_obs = pm.Categorical('y_obs', p=thetas, observed=y_subset)
+	    thetas = pm.Deterministic('theta', T.nnet.softmax(linear_combi))
+	    # Y commes from a Categorical(thetas)
+	    y_obs = pm.Categorical('y_obs', p=thetas, observed=out_last_hidden_layer)
 	    trace = pm.sample(5000,chains=2)
 	return trace
 
@@ -26,6 +26,11 @@ def pymc3_sampling(D, sigma_in, out):
 
 
 ###### A faire #############
+# fix HMC et faire pymc3 (NUTs) mais apres le mieux c'est tensorflow ou Pytorch
+
+
 # tensorflow: tenosrflowprobability OU PytorchPyro 
 # Ou sinon: numpyro
-# Aussi faire BBVI, 
+
+
+# Aussi faire BBVI 
