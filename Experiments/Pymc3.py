7347e1419c988a3425193597b82aca0223213ce8
RaphaelPellegrin <raphael.pellegrin74@gmail.com>
2020-11-24 16:23:41 +0000

    change pymc3

--- Experiments/Pymc3.py
+++ Experiments/Pymc3.py
@@ -7,29 +7,30 @@ import pymc3 as pm
 import theano.tensor as T
 
 
-def pymc3_sampling(D=5, sigma_in=1, out_last_hidden_layer, output_dim=3, out_y):
-	"""
-	INPUTS:
-	D: dimension of the last hidden layer
-	sigma_in: std of the prior (a normal, with mean 0)
 
-	OUTPUTS:
-	"""
-	with pm.Model() as replacing_HMC:  
-	    # w has a prior: N(0,1) 
-	    # Output dim number of bias
-	    w = pm.Normal('w', mu=0, sigma=sigma_in, shape=(D*output_dim+output_dim)) 
-	    linear_combinations=[]
-	    for j in range(output_dim):
-	    	linear_combi = pm.math.dot(out_last_hidden_layer,w[j:j*D])+w[-j]
-	    	linear_combinations.append(linear_combi)
-	    thetas = pm.Deterministic('theta', T.nnet.softmax(linear_combinations))
-	    # or thetas = pm.Deterministic('theta', pm.math.softmax(linear_combi))?
-	    # Y commes from a Categorical(thetas)
-	    y_obs = pm.Categorical('y_obs', p=thetas, observed=out_y)
-	    trace = pm.sample(5000,chains=2)
+def pymc3_sampling(D, mu_wanted=0, tau_wanted=1, out_last_hidden_layer, output_dim, out_y, samples_wanted=1000, number_chains=2):
+    """
+    INPUTS:
+    D: dimension of the last hidden layer
+    sigma_in: std of the prior (a normal, with mean 0)
+    I AM USING THE CONVENTION THAT THE BIAS COME AT THE END
 
-	return trace
+    OUTPUTS:
+    """
+    with pm.Model() as replacing_HMC:  
+        # w has a prior: N(0,1) 
+        # Output dim number of bias
+        w = pm.Normal('w', mu=mu_wanted, tau=tau_wanted, shape=(D*output_dim+output_dim)) 
+        linear_combinations=[]
+        for j in range(output_dim):
+            dot=pm.math.dot(out_last_hidden_layer[0].T,w[j*D:j*D+D])+w[-j]
+            linear_combi = pm.Deterministic('s'+str(j),dot)
+            linear_combinations.append(linear_combi)
+        thetas = pm.Deterministic('theta', T.nnet.softmax(linear_combinations))
+        # Y commes from a Categorical(thetas)
+        y_obs = pm.Bernoulli('y_obs', p=thetas, observed=out_y)
+        trace = pm.sample(samples_wanted,chains=number_chains)
+    return trace
 
 
 
