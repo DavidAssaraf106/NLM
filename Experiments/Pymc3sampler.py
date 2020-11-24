5a13e86e5547c0f23c704b2d590132cf9d27df4d
RaphaelPellegrin <raphael.pellegrin74@gmail.com>
2020-11-24 17:47:48 +0000

    sampler

--- Experiments/Pymc3sampler.py
+++ Experiments/Pymc3sampler.py
@@ -1,10 +1,10 @@
 import autograd.numpy as np
-import autograd.numpy as np
 import matplotlib.pyplot as plt
 import warnings
 from pymc3 import Model
 import pymc3 as pm
 import theano.tensor as T
+from statsmodels.graphics.tsaplots import plot_acf
 
 
 
@@ -16,6 +16,7 @@ def pymc3_sampling(out_last_hidden_layer, output_dim, out_y, D, mu_wanted=0, tau
     I AM USING THE CONVENTION THAT THE BIAS COME AT THE END
 
     OUTPUTS:
+    the trace
     """
     with pm.Model() as replacing_HMC:  
         # w has a prior: N(0,1) 
@@ -33,21 +34,33 @@ def pymc3_sampling(out_last_hidden_layer, output_dim, out_y, D, mu_wanted=0, tau
     return trace
 
 
-def plot_trace(traces, weight_number=1, thin_wanted=3, burn_wanted=0.2, colour='b'):
-	burn_in=burn_wanted
-	thinning_factor=thin_wanted
 
 
+##### A FINIR ###########################################
+# def plot_trace(traces, weight_number=1, thin_wanted=3, burn_wanted=0.2, colour='b', lag=20):
+#     """
+#     INPUTS:
+#     the trace, the weight number we want to plot, the thin and burn_in rates, the colour,
+#     the lag for the autocorrelation plots
 
-	w=[]
-	for i in range(len(traces['w'])):    
-	    w.append(traces['w'][i][weight_number])
 
-	wp = w[int(burn_in*len(traces)):][::thinning_factor]        
-	    
-	fig, ax=plt.subplots(1,1,figsize=(20,5))
-	ax.plot(range(len(wp)), wp, color=colour)
-	ax.set_title('Trace plot for weight {}'.format(weight_number))
+#     OUTPUTS:
+#     the plots of the trace
+#     the autocorrelation plot
+
+#     """
+
+#     burn_in=burn_wanted
+#     thinning_factor=thin_wanted
+#     w=[]
+#     for i in range(len(traces['w'])):    
+#         w.append(traces['w'][i][weight_number])
+#     wp = w[int(burn_in*len(traces)):][::thinning_factor]  
+#     fig, ax=plt.subplots(1,1,figsize=(20,5))
+#     ax.plot(range(len(wp)), wp, color=colour)
+#     ax.set_title('Trace plot for weight {}'.format(weight_number))
+#     plot_acf(wp, lags=lag, label='w{}'.format(weight_number))
+
 
 
 
