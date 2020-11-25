Modifications: - Integrate the computation of epistemic uncertainty - Implement MCMC sampler diagnostics Bugs to be fixed: - Wrong formula in pymc3 for the use of the weights - Need to make sure we agree on the order of the weights Work to be done before having a functional NLM: - Make sure the way we get the weights + exploit them is ok - Perform unit testing on NLM

Experiments/Neural_Network.py
@@ -1,20 +1,11 @@
-from FeedForwardNN import Feedforward
-from Toy_Datasets import two_clusters_gaussian
 import autograd.numpy as np
 from autograd import grad
 from autograd.misc.optimizers import adam
-from Bayesian_pdf import get_log_prior, get_log_likelihood
-from Hamiltonian_MC import hmc
-from scipy.special import softmax as sftmax
-import autograd.numpy as np
-# import matplotlib.pyplot as plt
-import warnings
-from pymc3 import Model
 import pymc3 as pm
 import theano.tensor as T
 import random
-
-
+from Bayesian_pdf import get_log_prior, get_log_likelihood
+from Hamiltonian_MC import hmc
 
 
 def sigmoid(z):
@@ -29,7 +20,8 @@ def softmax(y):  # checked, ok for softmax and the dimensions
     D = y.shape[1]
     z = y.flatten().reshape(D, -1)
     z = np.exp(z)
-    return z/np.sum(z, axis=0)
+    return z / np.sum(z, axis=0)
+
 
 class NLM:
     """
@@ -85,9 +77,8 @@ class NLM:
         self.objective_trace = np.empty((1, 1))
         self.weight_trace = np.empty((1, self.D))
 
-    # todo: fix a nan in gradient
     def forward(self, weights, x, partial=False):
-        ''' Forward pass given weights and input '''
+        """ Forward pass given weights and input """
         H = self.params['H']
         D_in = self.params['D_in']
         D_out = self.params['D_out']
@@ -132,7 +123,6 @@ class NLM:
         assert output.shape[1] == self.params['D_out']
 
         return output
-
 
     def make_objective(self, x_train, y_train, reg_param):
         # We are in the case of multi-task classification. The labels need to be one-hot encoded and the loss function we
@@ -226,16 +216,16 @@ class NLM:
         for every layer. Therefore, we need to reshape our weights here.
         NOTE: works for achitecture with > 1 hidden layer
         """
-        index_output_layer = - self.params['H']*self.params['D_out'] - self.params['D_out']
+        # todo: make sure we agree on this point
+        index_output_layer = - self.params['H'] * self.params['D_out'] - self.params['D_out']
         weights_concerned = self.weights.flatten()[index_output_layer:]
         weights_reshape = []
         for d in range(self.params['D_out']):
-            weights_node_d = list(weights_concerned[d*self.params['H']:(d+1)*self.params['H']])
-            bias_node_d = weights_concerned[self.params['H']*self.params['D_out']+d]
+            weights_node_d = list(weights_concerned[d * self.params['H']:(d + 1) * self.params['H']])
+            bias_node_d = weights_concerned[self.params['H'] * self.params['D_out'] + d]
             weights_node_d.append(bias_node_d)
             weights_reshape.append(weights_node_d)
         return np.array(weights_reshape).flatten()
-
 
 
     def pymc3_sampling(self, out_last_hidden_layer, output_dim, y, D, mu_wanted=0, tau_wanted=1, samples_wanted=1000,
@@ -252,6 +242,7 @@ class NLM:
         :return: samples from the posterior of the Bayesian Logistic regression
         """
         initialization_pymc3 = self.get_feature_map_weights()
+        # todo: fix the way we use the weights inside this function
         with pm.Model() as replacing_HMC:
             w = pm.Normal('w', mu=initialization_pymc3, tau=tau_wanted, shape=(D * output_dim + output_dim))
             linear_combinations = []
@@ -282,7 +273,8 @@ class NLM:
         :return: Samples from the posterior distribution sampled via the NUTS pymc3.
         """
         D = self.params['H']  # dimensionality of the feature map
-        samples = self.pymc3_sampling(self.forward(self.weights, x_train, partial=True), self.params['D_out'], y_train, D)
+        samples = self.pymc3_sampling(self.forward(self.weights, x_train, partial=True), self.params['D_out'], y_train,
+                                      D)
         return samples['w']
 
 
@@ -291,23 +283,55 @@ class NLM:
         self.fit_MLE(x_train, y_train, params_fit)
         print('NN trained ! Now, thanks to the feature map, we are going to sample the posterior weights')
         samples = self.fit_NLM(x_train, y_train)
+        print('Posterior samples sampled !')
         return samples
 
 
     def sample_models(self, x_train, y_train, params_fit, num_models):
-        posterior_weights = np.array(self.sample_posterior(x_train, y_train, params_fit))  # size : (num_weights, num_samples)
+        posterior_weights = np.array(
+            self.sample_posterior(x_train, y_train, params_fit))  # size : (num_weights, num_samples)
+        print('Now, thanks to the posterior, we are going to create ' + str(
+            num_models) + 'different classification models')
         indexes_chosen = random.choices(posterior_weights.shape[1], k=num_models)
         selected_weights = posterior_weights[:, indexes_chosen]  # size : (num_weights, num_models)
         # one thing we need to solve about the weights is that this is not the right shape, we expect weights + biases and
-        # what we are doing rn is
+        # todo: fix the way we add the weights, if it's okay with the shape
         index_output_layer = - self.params['H'] * self.params['D_out'] - self.params['D_out']
         weights_independent = self.weights[:index_output_layer]
         models = []
         for weight in selected_weights:
-            complete_weight_model = np.array(list(weights_independent) + list(weight))
+            complete_weight_model = np.array(
+                list(weights_independent.flatten()) + list(weight.flatten()))  # make sure we get this right
+            complete_weight_model = complete_weight_model.reshape((1, -1))
+            models.append(Classifier(complete_weight_model, self.forward))
+        return models
 
 
-
+    def uncertainty_computation(self, models, points, test_points=None):
+        """
+        This function computes the epistemic uncertainty of points in test_points, as is done in ...
+        For now, we do not use the entropy or the expectanty, we just get a rought approximate of the epistemic uncertainty
+        by adding the variance of the probability predictions for every class
+        :param test_points: points for which we want to calculate the uncertainty
+        :param models: sample weights from the posterior (for the last hidden layer to output):
+        this gives a NN with fixed weights, which to each input associates an output (vector of probabilities): this is one model
+        models is a list [model1, model2, ...]. They should we created via the Classifier class
+        :return: The epistemic uncertainty for each point intest_points, as a list
+        """
+        train_epistemic_uncer = []
+        for point in points:
+            list_p = []
+            for i in range(len(models)):
+                list_p.append(models[i].predict_proba(np.array(point).reshape(1, -1)))
+            train_epistemic_uncer.append(np.sum(np.std(np.array(list_p), axis=1)))
+        test_epistemic_uncer = []
+        if test_points:
+            for point in test_points:
+                list_p = []
+                for i in range(len(models)):
+                    list_p.append(models[i].predict_proba(np.array(point).reshape(1, -1)))
+                test_epistemic_uncer.append(np.sum(np.std(np.array(list_p), axis=1)))
+        return train_epistemic_uncer, test_epistemic_uncer
 
 
 
@@ -315,6 +339,7 @@ class Classifier:
     """
     This class implements the scikit-learn API for our Neural Network.
     """
+
     def __init__(self, weights, forward):
         self.weights = weights
         self.forward = forward