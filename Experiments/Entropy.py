Modifications: - Fixed the computation of the epistemic uncertainty - Added a description of the experiment to be done in order to show that the training for NLM is unsuitable Bug: - Epistemic uncertainty plots are not right

Experiments/Entropy.py
@@ -4,46 +4,43 @@ import warnings
 from statsmodels.graphics.tsaplots import plot_acf
 
 
+
 def entropy_vec(v):
-	D=len(v)
-	su=0
-	for i in range(D):
-		if v[i]==0:
-			su+=0
-		else:
-			su+=np.log(v[i])*v[i]
-	return -su
+    if np.min(v) == 0:
+        v += 1e-15
+    return -np.sum(np.log(v) * v)
 
 
 def total_uncertainty(x, models, n_iter=100):
-	"""
+    """
 	models is a list of objects of class classifiers
 	"""
-	# Step 1: estimate the expectation
-   	s=0
-   	for mod in models:
-		s+=mod.predict_proba(x) # a vector in R^(output_dim)
-    # Step 2: entropy
-	return entropy_vec(s/len(models))
+    # Step 1: estimate the expectation
+    s = 0
+    for mod in models:
+        x = x.reshape(1, 2)
+        s += mod.predict_proba(x)  # a vector in R^(output_dim)
+    return entropy_vec(s / len(models))
 
 
 def expected_aleatoric_uncertainty(x, models, n_iter=100):
-	"""
+    """
 	models is a list of objects of class classifiers
 	"""
-	m=len(models)
- 	s=0
-	for mod in models:
-		a=mod.predict_proba(x)
-		s+=entropy_vec(a)
-	return s/m
+    m = len(models)
+    s = 0
+    for mod in models:
+        x = x.reshape(1, 2)
+        a = mod.predict_proba(x)
+        s += entropy_vec(a)
+    return s / m
 
 
 def epistemic_uncertainty(x, models, n_iter=100):
-	"""
+    """
 	models is a list of objects of class classifiers
 
 	OUTPUT:
 	The Mutual information (MI) 
 	"""
-	return total_uncertainty(x, models,n_iter=100)-expected_aleatoric_uncertainty(x, models,n_iter=100)
+    return total_uncertainty(x, models, n_iter=100) - expected_aleatoric_uncertainty(x, models, n_iter=100)