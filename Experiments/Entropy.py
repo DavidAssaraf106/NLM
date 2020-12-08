8148f6da942fa726e97f891b1e9bdd7249d54739
RaphaelPellegrin <raphael.pellegrin74@gmail.com>
2020-12-08 00:57:03 +0000

    removed untitled notebooks

--- Experiments/Entropy.py
+++ Experiments/Entropy.py
@@ -7,21 +7,26 @@ def entropy_vec(v):
 	D=len(v)
 	su=0
 	for i in range(D):
-		su+=np.log(v[i])*v[i]
+		if v[i]==0:
+			su+=0
+		else:
+			su+=np.log(v[i])*v[i]
 	return -su
 
 
-# TO DO
-def total_uncertainty(y,trace,x):
-#   trace_subset=take subset of trace
-#   m number of weights samples in trace)subset
-#   s=0
-#   for weights in trace_subset:
-#			s+=p(y|x^*,W)/m
-	return entropy_vec(s)
+def total_uncertainty(x,n_iter=100, models):
+	"""
+	models is a list of objects of class classifiers
+	"""
+	# Step 1: estimate the expectation
+   	s=0
+   	for mod in models:
+        s+=mod.predict_proba(x) # a vector in R^(output_dim)
+    # Step 2: entropy
+	return entropy_vec(s/len(models))
 
 # TO DO
-def expected_aleatoric_uncertainty(y,trace,x):
+def expected_aleatoric_uncertainty(x,n_iter=100, models):
 #   trace_subset=take subset of trace
 #   m number of weights samples in trace)subset
 # 	s=0
@@ -31,5 +36,5 @@ def expected_aleatoric_uncertainty(y,trace,x):
 	return s/m
 
 
-def epistemic_uncertainty(y,trace,x):
-	return total_uncertainty(y,trace,x)-expected_aleatoric_uncertainty(y,trace,x)
+def epistemic_uncertainty(x,n_iter=100, models):
+	return total_uncertainty(x,n_iter=100, models)-expected_aleatoric_uncertainty(x,n_iter=100, models)
